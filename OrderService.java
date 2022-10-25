bf2f384325df7e15d7f5639cd4ae5267300c71d5
Lê Duy Hoàng <107383266+HoangLD15502@users.noreply.github.com>
2022-10-25 21:45:53 +0700

    update order

---
+++ OrderService.java
@@ -0,0 +1,21 @@
+package com.java.service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.java.entity.Order;
+import com.java.entity.OrderDetail;
+
+import java.util.List;
+
+public interface OrderService {
+    List<Order> findAll();
+
+    List<OrderDetail> findByMaHD(Long maHD);
+
+    Order update(Order order);
+
+    Order create(JsonNode orderData);
+
+	Order findById(Long maHD);
+
+	List<Order> findByUsername(String tenND);
+}
